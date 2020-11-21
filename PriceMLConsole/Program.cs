e4550abba047fcf18555e2703f95494edfbf7d5c
Abbe-Al-shammari <abbas1995abbe@gmail.com>
2020-11-21 08:33:15 +0300

    first

---
+++ PriceMLConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PriceMLConsoleML.Model;
+
+
+namespace PriceMLConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+
+            ModelOutput output = ConsumeModel.Predict(input);
+            Console.WriteLine($"Input Info");
+        }
+    }
+}
